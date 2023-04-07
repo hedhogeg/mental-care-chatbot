cd52507cb376479f795e4135fd509c3ef3e1e7e5
hedhogeg <hedhogeg@yonsei.ac.kr>
2023-04-07 20:28:43 +0900

    requirement.txt & web.py

---
+++ web.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+# from chatbot import get_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# @app.route('/chatbot', methods=['POST'])
+# def chatbot_response():
+#     user_input = request.form['user_input']
+#     bot_response = get_response(user_input)
+#     return str(bot_response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
